a7d4d34ad094677d16a147f1fab58f6ccebe8fe0
samuelmarina <samuellmarina@gmail.com>
2021-04-19 02:15:08 -0400

    Include calculator screen

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CalculatorScreen from './app/screens/Calculator/CalculatorScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CalculatorScreen />
     </View>
   );
 }
@@ -14,8 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
