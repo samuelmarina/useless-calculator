c6b41c30377128de65d68ee31211ae51959511b9
samuelmarina <samuellmarina@gmail.com>
2021-04-18 22:21:05 -0400

    Create button

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import colors from "../../config/colors";
 
-function Button(props) {
+function Button({
+    color = colors.gray,
+    text,
+}) {
     return (
-        <View style={styles.container}></View>
+        <TouchableHighlight style={[styles.container, {backgroundColor: color}]}>
+            <Text style={styles.text}>+</Text>
+        </TouchableHighlight>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        width: Dimensions.get('window').width * 0.2,
+        height: Dimensions.get('window').width * 0.2,
+        marginHorizontal: Dimensions.get('window').width * 0.02,
+    },
+    text: {
+        fontSize: 40,
+        color: "white",
     }
 })
 
