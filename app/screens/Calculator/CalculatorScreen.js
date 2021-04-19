43b8f7415260428f870156e95eed309bf9357e83
samuelmarina <samuellmarina@gmail.com>
2021-04-19 02:14:07 -0400

    Create main screen

--- app/screens/Calculator/CalculatorScreen.js
+++ app/screens/Calculator/CalculatorScreen.js
@@ -1,20 +1,112 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Result from '../../components/Result/Result';
+import React, { useState } from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+
+import Button from '../../components/Button/Button';
+import colors from "../../config/colors";
+import Context from "../../helpers/context/context";
+import Row from "../../components/Row/Row";
+import { initialState } from "../../helpers/calculator/calculator";
 
 function CalculatorScreen(props) {
+    const [result, setResult] = useState(initialState);
+    
     return (
-        <View style={styles.container}>
-            <Result result={123}/>
-        </View>
+        <Context.Provider value={{ result, setResult }}>
+            <View style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <SafeAreaView>
+                    <Text style={styles.value}>
+                        {result.currentValue}
+                    </Text>
+                    <Row>
+                        <Button
+                            type="clear"
+                        >
+                            c
+                        </Button>
+                        <Button
+                            type="neg"
+                        >
+                            +/-
+                        </Button>
+                        <Button
+                            type="percentage"
+                        >
+                            %
+                        </Button>
+                        <Button 
+                            color={colors.orange}
+                            type="operator"
+                        >
+                            ÷
+                        </Button>
+                    </Row>
+                    <Row>
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button 
+                            color={colors.orange}
+                            type="operator"
+                        >
+                            x
+                        </Button>
+                    </Row>
+                    <Row>
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button 
+                            color={colors.orange}
+                            type="operator"  
+                        >
+                            +
+                        </Button>
+                    </Row>
+                    <Row>
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button type="num" />
+                        <Button 
+                            color={colors.orange}
+                            type="operator" 
+                        >
+                            -
+                        </Button>
+                    </Row>
+                    <Row>
+                        <Button 
+                            size="double"
+                            type="num"
+                        />
+                        <Button>.</Button>
+                        <Button 
+                            color={colors.orange}
+                            type="equal" 
+                        >
+                            =
+                        </Button>
+                    </Row>
+                </SafeAreaView>
+            </View>
+        </Context.Provider>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "black",
-        height: "100%",
-        width: "100%"
+        flex: 1,
+        justifyContent: "flex-end",
+        width: "100%",
+        paddingBottom: 10,
+    },
+    value: {
+        color: colors.text,
+        fontSize: 50,
+        textAlign: "right",
+        marginRight: 20,
+        marginBottom: 10
     }
 })
 
