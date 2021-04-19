d4bed2001914739142c5f545feabcc4718cf6e9b
samuelmarina <samuellmarina@gmail.com>
2021-04-18 20:22:00 -0400

    Initialize components

---
+++ app/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+function Button(props) {
+    return (
+        <View style={styles.container}></View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
+
+export default Button;
